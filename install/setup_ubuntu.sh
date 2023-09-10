f65d3c5fcddfb3b4853ed55aa5e69b56b136bc04
david-allan-jones <davej1094@yahoo.com>
2023-09-10 14:15:18 +0900

    Add colors to output

--- install/setup_ubuntu.sh
+++ install/setup_ubuntu.sh
@@ -2,27 +2,31 @@
 
 export DOTFILES=$(pwd -P)
 
-writeGreen() {
-	local GREEN = "\033[03m"
-	local RESET = "\033[39M"
-	echo -e "$GREEN $1 $RESET"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+RESET="\033[0m"
+
+writeColor() {
+	echo -e "$1 $2 $RESET"
 }
 
 run_installs() {
-  for dir in */; do
-    if [[ -d "$dir" && "$dir" != "bin/" && "$dir" != "install/" ]]; then
-      if [[ -f "$dir/install.sh" ]]; then
-        $dir./install.sh
-      fi
-    fi
-  done
+	for dir in */; do
+		if [[ -d "$dir" && "$dir" != "bin/" && "$dir" != "install/" ]]; then
+			if [[ -f "$dir/install.sh" ]]; then
+				writeColor $YELLOW "Installing $dir..."
+				$dir./install.sh
+				writeColor $YELLOW "Finished installing $dir"
+			fi
+		fi
+	done
 }
 
 run() {
-	writeGreen "Beginning setup..."
+	writeColor $GREEN "Beginning setup..."
 	sudo apt-get update
 	run_installs
-	writeGreen "Setup complete!"
+	writeColor $GREEN "Setup complete!"
 }
 
 run
