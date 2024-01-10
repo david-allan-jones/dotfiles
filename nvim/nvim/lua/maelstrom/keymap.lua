e6162a34843e9fbc5956fbd930b93786d992a06c
david-allan-jones <davej1094@yahoo.com>
2024-01-10 21:45:18 +0900

    keep search terms in the middle

--- nvim/nvim/lua/maelstrom/keymap.lua
+++ nvim/nvim/lua/maelstrom/keymap.lua
@@ -15,3 +15,5 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<C-p>', ':Ex<Enter>')
 
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
