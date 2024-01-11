5deff423a725112550ebeb3975f658323d36e7b6
david-allan-jones <davej1094@yahoo.com>
2024-01-11 21:15:39 +0900

    Add various keymaps

--- nvim/nvim/lua/maelstrom/keymap.lua
+++ nvim/nvim/lua/maelstrom/keymap.lua
@@ -1,9 +1,16 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.g.mapleader = ' '
 
+-- Misc
 vim.keymap.set('i', 'jj', '<Esc>')
 vim.keymap.set('n', '<Esc><Esc>', ':noh<CR><Esc>')
 
+-- Paste without overwriting 
+vim.keymap.set('v', 'p', '"_dP')
+
+-- Yank to end of line
+vim.keymap.set('n', 'Y', 'y$')
+
+-- NOPs
 vim.keymap.set('n', '<Up>', '<NOP>')
 vim.keymap.set('n', '<Down>', '<NOP>')
 vim.keymap.set('n', '<Left>', '<NOP>')
@@ -14,6 +21,17 @@ vim.keymap.set('n', '<C-b>', '<C-b>zz')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<C-p>', ':Ex<Enter>')
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
+-- Buffer/Window Shortcuts
+vim.keymap.set('n', '<leader>pp', vim.cmd.bp)
+vim.keymap.set('n', '<leader>nn', vim.cmd.bn)
+vim.keymap.set('n', '<leader>dd', vim.cmd.bd)
+vim.keymap.set('n', '<leader>jj', '<C-w>j')
+vim.keymap.set('n', '<leader>kk', '<C-w>k')
+vim.keymap.set('n', '<leader>hh', '<C-w>h')
+vim.keymap.set('n', '<leader>ll', '<C-w>l')
+
+-- Search Utility
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
