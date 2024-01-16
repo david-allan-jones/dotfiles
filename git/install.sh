55d30ef0472139bd9e6c5a75473724780fcde627
david-allan-jones <davej1094@yahoo.com>
2024-01-16 21:14:03 +0900

    Remove dead code and fix git install

--- git/install.sh
+++ git/install.sh
@@ -1,2 +1,2 @@
-git config --global core.excludesfile $DOTFILES/git/.gitignore
+git config --global core.excludesfile $(pwd)/git/.gitignore
 git config --global core.editor vim
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export DOTFILES=$(pwd)
-
 GREEN="\033[32m"
 YELLOW="\033[33m"
 RED="\033[31m"
