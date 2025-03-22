c076b10c54e1a57fe9f78629ddec010908c69ce7
David Jones <davej1094@yahoo.com>
2025-03-22 22:25:30 +0900

    Update nvim config

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,8 +1,5 @@
 require 'core'
 require 'plugins'
 
--- For things I don't want to version and be machine specific
-require 'custom'
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- Local environment specific (work, etc). Don't track
+require 'locals'
