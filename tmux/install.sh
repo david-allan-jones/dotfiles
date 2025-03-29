bb63c9fe28196ef21e2d76589c010f7070f355b1
David Jones <davej1094@yahoo.com>
2025-03-29 14:02:19 +0900

    Separate out tmux options

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,3 +4,4 @@ sudo apt install tmux
 ln -s $(pwd)/tmux/.tmux.conf ~/.tmux.conf
 ln -s $(pwd)/tmux/.tmux.keybinds ~/.tmux.keybinds
 ln -s $(pwd)/tmux/.tmux.display ~/.tmux.display
+ln -s $(pwd)/tmux/.tmux.options ~/.tmux.options
