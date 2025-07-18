919d8e20453a74a5ae6fdf3acad0904fca742cf8
david-allan-jones <davej1094@yahoo.com>
2025-07-18 14:00:05 +0900

    Flatten nvim

--- nvim/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo rm -rf ~/.config/nvim
-sudo rm /usr/local/bin/nvim
-
-sudo apt purge -y libfuse2
--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Bash
-ln -s $(pwd)/bash/.bash_profile ~/.bash_profile
-ln -s $(pwd)/bash/.bashrc ~/.bashrc
+ln -s $(pwd)/.bash_profile ~/.bash_profile
+ln -s $(pwd)/.bashrc ~/.bashrc
 
 # Git
-ln -s $(pwd)/git/.gitconfig ~/.gitconfig
-ln -s $(pwd)/git/.gitignore ~/.gitignore
-
-# Vim
-ln -s $(pwd)/vim/.vimrc ~/.vimrc
+ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/.gitignore ~/.gitignore
 
 # Tmux
-ln -s $(pwd)/tmux/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/tmux/.tmux.keybinds ~/.tmux.keybinds
-ln -s $(pwd)/tmux/.tmux.display ~/.tmux.display
-ln -s $(pwd)/tmux/.tmux.options ~/.tmux.options
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+
+# Vim
+ln -s $(pwd)/.vimrc ~/.vimrc
+
+# Neovim
+ln -s $(pwd)/nvim ~/.config/nvim
