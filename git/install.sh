2a46b1fdfdcf99274d61802201bf56d0a43adbae
david-allan-jones <davej1094@yahoo.com>
2025-07-18 13:50:35 +0900

    Flatten git

--- git/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ln -s $(pwd)/git/.gitconfig ~/.gitconfig
-ln -s $(pwd)/git/.gitignore ~/.gitignore
--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Bash
-# Ask to setup bash. Prompt the user
-echo "Setting up bash environment..."
-
 ln -s $(pwd)/bash/.bash_profile ~/.bash_profile
 ln -s $(pwd)/bash/.bashrc ~/.bashrc
+
+# Git
+ln -s $(pwd)/git/.gitconfig ~/.gitconfig
+ln -s $(pwd)/git/.gitignore ~/.gitignore
